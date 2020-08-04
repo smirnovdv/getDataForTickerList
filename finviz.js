3cab6d1aa6b279a2e9c98da2a3fca6b7a527275a
smirnovdv <sdmit@yandex.ru>
2020-08-03 22:50:25 -0500

    single object output added

--- finviz.js
+++ finviz.js
@@ -2,14 +2,17 @@ const puppeeter = require('puppeteer');
 const csv = require('csv-parser')
 const fs = require('fs')
 const results = [];
+const dataArray = [];
 
-async function scrapFinviz(tickers){
-  const browser = await puppeeter.launch();
+async function scrapFinviz(tickers) {
+  const browser = await puppeeter.launch({headless: false});
   const page = await browser.newPage();
-  for (let i=0;i<tickers.length;i++) {
+  for (let i = 0; i < tickers.length; i++) {
     let data = {};
     console.log(`https://finviz.com/quote.ashx?t=${tickers[i][ '﻿Symbol']}`)
+    await page.setDefaultNavigationTimeout(0);
     await page.goto(`https://finviz.com/quote.ashx?t=${tickers[i][ '﻿Symbol']}`);
+    data.ticker = tickers[i][ '﻿Symbol']
     data.mcap = await page.$eval(`body > table:nth-child(5) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(7) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > b`, e => e.innerText).catch(console.error);
     data.ernDate = await page.$eval(`body > table:nth-child(5) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(7) > td > table > tbody > tr:nth-child(11) > td:nth-child(6) > b`, e => e.innerText).catch(console.error);
     data.instOwn = await page.$eval(`body > table:nth-child(5) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(7) > td > table > tbody > tr:nth-child(3) > td:nth-child(8) > b`, e => e.innerText).catch(console.error);
@@ -18,10 +21,10 @@ async function scrapFinviz(tickers){
     data.insiderOwn = await page.$eval(`body > table:nth-child(5) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(7) > td > table > tbody > tr:nth-child(1) > td:nth-child(8) > b `, e => e.innerText).catch(console.error);
     //data.lastNews =  await page.$eval(`#news-table > tbody > tr:nth-child(1) > td:nth-child(2) > div > div.news-link-left > a`, e => e.innerText);
     //data.lastNewsDate = await page.$eval(`#news-table > tbody > tr:nth-child(1) > td:nth-child(1)`, e => e.innerText);
-    console.log(data);
+    dataArray.push(data); //TODO: right to scv here
   };
   await browser.close();
-
+  console.log(dataArray)
 };
 
 fs.createReadStream('ts-report.csv')
@@ -35,5 +38,3 @@ fs.createReadStream('ts-report.csv')
 
 
 //scrapFinviz('MIST');
-
- 
